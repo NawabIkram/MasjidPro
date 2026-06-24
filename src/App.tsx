081a72233a74906813563413b1c611e8457a6621
NawabIkram <nawabikram786@gmail.com>
2026-06-24 17:31:03 +0500

    feat: wire Phase 2 routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { AppShell } from "./components/AppShell";
 import { AnnouncementsPage } from "./pages/AnnouncementsPage";
+import { CheckoutPage } from "./pages/CheckoutPage";
 import { DashboardPage } from "./pages/DashboardPage";
+import { DemoPage } from "./pages/DemoPage";
 import { DonationsPage } from "./pages/DonationsPage";
 import { DonorPortalPage } from "./pages/DonorPortalPage";
 import { DonorsPage } from "./pages/DonorsPage";
+import { LaunchPage } from "./pages/LaunchPage";
 import { LoginPage } from "./pages/LoginPage";
+import { MarketingPage } from "./pages/MarketingPage";
 import { PrayerTimesPage } from "./pages/PrayerTimesPage";
+import { PricingPage } from "./pages/PricingPage";
 import { ReportsPage } from "./pages/ReportsPage";
 import { SettingsPage } from "./pages/SettingsPage";
 import { ZakatPage } from "./pages/ZakatPage";
@@ -14,10 +19,14 @@ import { ZakatPage } from "./pages/ZakatPage";
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<MarketingPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/demo" element={<DemoPage />} />
+      <Route path="/checkout/:planId" element={<CheckoutPage />} />
       <Route element={<AppShell />}>
-        <Route index element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/launch" element={<LaunchPage />} />
         <Route path="/donations" element={<DonationsPage />} />
         <Route path="/zakat" element={<ZakatPage />} />
         <Route path="/prayer-times" element={<PrayerTimesPage />} />
